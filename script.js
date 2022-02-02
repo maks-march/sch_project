f270640808cc512125d75b73a19ea96842b00994
maks-march <1maks_2055@mail.ru>
2022-02-02 19:17:53 +0500

    superb

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const header = document.querySelectorAll('header');
-console.log(header);
-alert("doldd");
+const color_string = document.querySelector('strings');
+setColors(color_string);
+// setInterval(setColors,1000);
+
+function setColors(color_string) {
+    for (i=0;i<256;i++) {
+        color_string.innerHTML = 
+        `<h1 class="header" style="color: rgb(255,0,${i})">статья о том как я писал эту статью</h1>`
+    }
+    for (i=255;i>-1;i--) {
+        color_string.innerHTML = 
+        `<h1 class="header" style="color: rgb(${i},0,255)">статья о том как я писал эту статью</h1>`
+    }
+    for (i=0;i<256;i++) {
+        color_string.innerHTML = 
+        `<h1 class="header" style="color: rgb(0,${i},255)">статья о том как я писал эту статью</h1>`
+    }
+    for (i=255;i>-1;i--) {
+        color_string.innerHTML = 
+        `<h1 class="header" style="color: rgb(0,255,${i})">статья о том как я писал эту статью</h1>`
+    }
+    for (i=0;i<256;i++) {
+        color_string.innerHTML = 
+        `<h1 class="header" style="color: rgb(${i},255,0)">статья о том как я писал эту статью</h1>`
+    }
+    for (i=255;i>-1;i--) {
+        color_string.innerHTML = 
+        `<h1 class="header" style="color: rgb(255,${i},0)">статья о том как я писал эту статью</h1>`
+    }
+}
