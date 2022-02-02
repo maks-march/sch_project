67c91fea7ddc5e193a2bdb80286b69f3eb749872
maks-march <1maks_2055@mail.ru>
2022-02-02 19:26:04 +0500

    A

--- script.js
+++ script.js
@@ -6,26 +6,26 @@ setColors(color_string);
 function setColors(color_string) {
     for (i=0;i<256;i++) {
         color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(255,0,${i})">статья о том как я писал эту статью</h1>`
+        `<h1 class="header" style="color: rgb(255,0,${i})">статья о том как я писал эту статью</h1>`;
     }
     for (i=255;i>-1;i--) {
         color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(${i},0,255)">статья о том как я писал эту статью</h1>`
+        `<h1 class="header" style="color: rgb(${i},0,255)">статья о том как я писал эту статью</h1>`;
     }
     for (i=0;i<256;i++) {
         color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(0,${i},255)">статья о том как я писал эту статью</h1>`
+        `<h1 class="header" style="color: rgb(0,${i},255)">статья о том как я писал эту статью</h1>`;
     }
     for (i=255;i>-1;i--) {
         color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(0,255,${i})">статья о том как я писал эту статью</h1>`
+        `<h1 class="header" style="color: rgb(0,255,${i})">статья о том как я писал эту статью</h1>`;
     }
     for (i=0;i<256;i++) {
         color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(${i},255,0)">статья о том как я писал эту статью</h1>`
+        `<h1 class="header" style="color: rgb(${i},255,0)">статья о том как я писал эту статью</h1>`;
     }
     for (i=255;i>-1;i--) {
         color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(255,${i},0)">статья о том как я писал эту статью</h1>`
+        `<h1 class="header" style="color: rgb(255,${i},0)">статья о том как я писал эту статью</h1>`;
     }
 }
