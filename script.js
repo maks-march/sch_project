b85f946bee09e30d93d2f50040fb412e73aabace
maks-march <1maks_2055@mail.ru>
2022-02-02 21:00:47 +0500

    м

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-const color_string = document.querySelector('rainbow');
-console.log(color_string);
-setColors(color_string);
-// setInterval(setColors,1000);
-
-function setColors(color_string) {
+setInterval(setColors(),6000);
+function setColors() {
     for (i=0;i<256;i++) {
-        color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(255,0,${i})">статья о том как я писал эту статью</h1>`;
+        setTimeout (setcolor1(i),1000);
     }
     for (i=255;i>-1;i--) {
-        color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(${i},0,255)">статья о том как я писал эту статью</h1>`;
+        setTimeout (setcolor2(i),1000);        
     }
     for (i=0;i<256;i++) {
-        color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(0,${i},255)">статья о том как я писал эту статью</h1>`;
+        setTimeout (setcolor3(i),1000);    
     }
     for (i=255;i>-1;i--) {
-        color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(0,255,${i})">статья о том как я писал эту статью</h1>`;
+         
+        setTimeout (setcolor4(i),1000);             
     }
     for (i=0;i<256;i++) {
-        color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(${i},255,0)">статья о том как я писал эту статью</h1>`;
+        setTimeout (setcolor5(i),1000);               
     }
     for (i=255;i>-1;i--) {
-        color_string.innerHTML = 
-        `<h1 class="header" style="color: rgb(255,${i},0)">статья о том как я писал эту статью</h1>`;
+        setTimeout (setcolor6(i),1000); 
     }
-}
+    };
+    function setcolor1(i){
+        document.querySelector('.rainbow').innerHTML = ``;
+        setInterval (() => {console.log(document.querySelector('.header'));},1000);
+        document.querySelector('.rainbow').innerHTML = `<h1 class="header" style="color: rgb(255,0,${i})">статья о том как я писал эту статью</h1>`;
+    }
+    function setcolor2(i){
+        document.querySelector('.rainbow').innerHTML = ``;
+        document.querySelector('.rainbow').innerHTML = `<h1 class="header" style="color: rgb(${i},0,255)">статья о том как я писал эту статью</h1>`;
+    }
+    function setcolor3(i){
+        document.querySelector('.rainbow').innerHTML = ``;
+        document.querySelector('.rainbow').innerHTML = `<h1 class="header" style="color: rgb(0,${i},255)">статья о том как я писал эту статью</h1>`;
+    }
+    function setcolor4(i){
+        document.querySelector('.rainbow').innerHTML = ``;
+        document.querySelector('.rainbow').innerHTML = `<h1 class="header" style="color: rgb(0,255,${i})">статья о том как я писал эту статью</h1>`;
+    }
+    function setcolor5(i){
+        document.querySelector('.rainbow').innerHTML = ``;
+        document.querySelector('.rainbow').innerHTML = `<h1 class="header" style="color: rgb(${i},255,0)">статья о том как я писал эту статью</h1>`;
+    }
+    function setcolor6(i){
+        document.querySelector('.rainbow').innerHTML = ``;
+        document.querySelector('.rainbow').innerHTML = `<h1 class="header" style="color: rgb(255,${i},0)">статья о том как я писал эту статью</h1>`;
+    }
